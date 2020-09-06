5d5ddf1b411c4bcaac01039d72af085a70df693c
lalit yadav <lalit15001001026@gmail.com>
2020-09-06 16:54:07 +0530

    added hello api view

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View """
+
+    def get(self,request,format=None):
+        """Returns a list of API View features"""
+        an_apiview=[
+        "its lalit yadav for the great learning",
+        "it is similar to normal http et and post request",
+        "give you most control over api logic"
+        ]
+
+        return Response({'message':"hello",'an_apiview':an_apiview})
